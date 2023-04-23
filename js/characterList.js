5de3d1b6a0e67e564a9a6fd4f425f329dcdb8924
Andrew Bungcayao <thedevandrew@gmail.com>
2023-04-23 00:20:58 -0400

    Update landing-page image path, utilize SWAPI api to dynamically generate Star Wars Characters on character list page with minor styling.

--- js/characterList.js
+++ js/characterList.js
@@ -1,5 +1,57 @@
-console.log("Hello World")
+const charactersContainerEl = document.querySelector(".characters-container");
+console.log(charactersContainerEl);
+const baseApi = "https://swapi.dev/api/";
+let currentPage = 1;
 
-const baseApi = 'https://swapi.dev/api/'
+const fetchCharacters = async () => {
+  try {
+    const charactersPageConfig = `${baseApi}people/?page=`;
 
-fetch(`${baseApi}people`)
+    const getCharactersPage = `${charactersPageConfig}${currentPage}`;
+    console.log(getCharactersPage);
+
+    const response = await fetch(getCharactersPage);
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+
+    for (i = 0; i < data.results.length; i++) {
+      const characterButtonEl = document.createElement("button");
+      console.log(characterButtonEl);
+      characterButtonEl.textContent = data.results[i].name;
+      characterButtonEl.classList.add('character-button')
+      charactersContainerEl.appendChild(characterButtonEl);
+    }
+
+    nextPage = data.next;
+    console.log(nextPage);
+    currentPage++;
+    if (nextPage === null) {
+      return console.log("All character pages rendered.");
+    } else {
+      fetchCharacters()
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchCharacters();
+
+// const generateCharacters = (data) => {
+
+//   console.log(data)
+// }
+
+// fetchCharactersData(fetchCharactersData())
+
+// ES5 form of fetching data through .then()
+
+// fetch(getCharacters).then(function (response) {
+//   console.log(response)
+//   return response.json()
+// }).then(function (data) {
+//   console.log(data)
+//   return data
+// })
