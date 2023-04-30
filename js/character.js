7baa6c0dcab9c7ce343a9492b02b49fe35023874
Andrew Bungcayao <thedevandrew@gmail.com>
2023-04-30 03:43:49 -0400

    Create a function to optimize character details for character page.

--- js/character.js
+++ js/character.js
@@ -27,37 +27,51 @@ const generateCharacter = async () => {
     characterNameEl.textContent = characterData.name;
     characterNameContainerEl.prepend(characterNameEl);
 
+    // Create function to for creating character details for better optimization.
+    const createCharacterDetails = (characterDetailEl, content) => {
+      characterDetailEl.textContent = content;
+      characterDetailsContainerEl.appendChild(characterDetailEl)
+    }
+
     const characterBornH2El = document.createElement("h2");
-    characterBornH2El.textContent = "DOB";
-    characterDetailsContainerEl.appendChild(characterBornH2El);
+    createCharacterDetails(characterBornH2El, "DOB");
+    // characterBornH2El.textContent = "DOB";
+    // characterDetailsContainerEl.appendChild(characterBornH2El);
 
     const characterBornValue = document.createElement("p");
-    characterBornValue.textContent = characterData.birth_year;
-    characterDetailsContainerEl.appendChild(characterBornValue);
+    createCharacterDetails(characterBornValue, characterData.birth_year);
+    // characterBornValue.textContent = characterData.birth_year;
+    // characterDetailsContainerEl.appendChild(characterBornValue);
 
     const characterGenderH2El = document.createElement("h2");
-    characterGenderH2El.textContent = "Gender";
-    characterDetailsContainerEl.appendChild(characterGenderH2El);
+    createCharacterDetails(characterGenderH2El, "Gender");
+    // characterGenderH2El.textContent = "Gender";
+    // characterDetailsContainerEl.appendChild(characterGenderH2El);
 
     const characterGenderValue = document.createElement("p");
-    characterGenderValue.textContent = characterData.gender;
-    characterDetailsContainerEl.appendChild(characterGenderValue);
+    createCharacterDetails(characterGenderValue, characterData.gender);
+    // characterGenderValue.textContent = characterData.gender;
+    // characterDetailsContainerEl.appendChild(characterGenderValue);
 
     const characterHairColorH2El = document.createElement("h2");
-    characterHairColorH2El.textContent = "Hair Color";
-    characterDetailsContainerEl.appendChild(characterHairColorH2El);
+    createCharacterDetails(characterHairColorH2El, "Hair Color");
+    // characterHairColorH2El.textContent = "Hair Color";
+    // characterDetailsContainerEl.appendChild(characterHairColorH2El);
 
     const characterHairColorValue = document.createElement("p");
-    characterHairColorValue.textContent = characterData.hair_color;
-    characterDetailsContainerEl.appendChild(characterHairColorValue);
+    createCharacterDetails(characterHairColorValue, characterData.hair_color);
+    // characterHairColorValue.textContent = characterData.hair_color;
+    // characterDetailsContainerEl.appendChild(characterHairColorValue);
 
     const characterEyeColorH2El = document.createElement("h2");
-    characterEyeColorH2El.textContent = "Eye Color";
-    characterDetailsContainerEl.appendChild(characterEyeColorH2El);
+    createCharacterDetails(characterEyeColorH2El, "Eye Color");
+    // characterEyeColorH2El.textContent = "Eye Color";
+    // characterDetailsContainerEl.appendChild(characterEyeColorH2El);
 
     const characterEyeColorValue = document.createElement("p");
-    characterEyeColorValue.textContent = characterData.eye_color;
-    characterDetailsContainerEl.appendChild(characterEyeColorValue);
+    createCharacterDetails(characterEyeColorValue, characterData.eye_color);
+    // characterEyeColorValue.textContent = characterData.eye_color;
+    // characterDetailsContainerEl.appendChild(characterEyeColorValue);
 
     const characterMoviesH2El = document.createElement("h2");
     // console.log(characterData.films.length)
